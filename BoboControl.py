7673797bc86b4153c0265969e996e4dae2277979
Alatec <johnsonkids.andrew@gmail.com>
2019-03-07 23:28:15 -0700

    More

--- BoboControl.py
+++ BoboControl.py
@@ -7,8 +7,8 @@ gamepad = InputDevice('/dev/input/event4')
 for event in gamepad.read_loop():
     if event.type == ecodes.EV_KEY:
         keyevent = categorize(event)
-        print(keyevent.keystate)
         if keyevent.keystate == KeyEvent.key_down:
+            print(keyevent.keycode)
             if keyevent.keycode == 'BTN_A':
                 print("Back")
             elif keyevent.keycode == 'BTN_Y':
