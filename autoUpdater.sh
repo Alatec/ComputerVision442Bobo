577ce3cd48dc72983b51b9eb35c57b9c8e6fbf74
Alatec <johnsonkids.andrew@gmail.com>
2019-03-08 16:55:01 -0700

    Enabled ssh connect on startup

--- autoUpdater.sh
+++ autoUpdater.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 cd /home/pi/Bobo/ComputerVision442
-export DISPLAY=:0.0
-while true;
-do
-if [[ $(git pull | tail -n +2) ]]; then
-    killall python3
-    python3 main.py
-fi
-sleep 1
-done
+
+ssh -R 6969:localhost:22 pi@laftr.ddns.net
+#while true;
+#do
+#if [[ $(git pull | tail -n +2) ]]; then
+#    killall python3
+#    python3 main.py
+#fi
+#sleep 1
+#done
