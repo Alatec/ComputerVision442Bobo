e2cb873adce5f3d59a535b38bd26b1cfe6d43314
Alatec <johnsonkids.andrew@gmail.com>
2019-03-07 23:42:19 -0700

    Morer

--- BoboControl.py
+++ BoboControl.py
@@ -20,6 +20,7 @@ headTilt = 6000
 motors = 6000
 turn = 6000
 
+
 for event in gamepad.read_loop():
     if event.type == ecodes.EV_KEY:
         keyevent = categorize(event)
@@ -30,12 +31,14 @@ for event in gamepad.read_loop():
                 if (motors < 1510):
                     motors = 1510
                 print(motors)
+                bobo.setTarget(MOTORS, motors)
             elif keyevent.keycode[1] == 'BTN_Y':
                 print("Triangle")
                 motors += 200
                 if (motors > 7900):
                     motors = 7900
                 print(motors)
+                bobo.setTarget(MOTORS, motors)
             elif keyevent.keycode[0] == 'BTN_B':
                 print("Circle")
 
