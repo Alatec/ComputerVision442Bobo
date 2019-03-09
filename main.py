8da3dcfdf29295640b0284de3bcb9fb993d99536
Alatec <johnsonkids.andrew@gmail.com>
2019-03-08 20:36:33 -0700

    track green

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     image = frame.array
     hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
     #hsv = cv2.medianBlur(hsv, 5)
-    orange = cv2.inRange(hsv, (0, 73, 210), (3, 116, 247))
+    orange = cv2.inRange(hsv, (54, 148, 134), (69, 184, 202))
     orange = cv2.cvtColor(orange, cv2.COLOR_GRAY2BGR)
     image = cv2.bitwise_and(orange, image)
     #pic = cv2.Canny(image, 100, 170)
