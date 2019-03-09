d63225c98a90ae400feecbe45f406ee6abdd2641
Alatec <johnsonkids.andrew@gmail.com>
2019-03-08 20:21:53 -0700

    aupdater test

--- autoUpdater.sh
+++ autoUpdater.sh
@@ -4,7 +4,7 @@ sleep 30
 ssh -i "/home/pi/.ssh/id_rsa" -R 6969:localhost:22 pi@laftr.ddns.net &
 while true;
 do
-if [[ $(git pull | tail -n +2) ]]; then
+if [ $(git pull | tail -n +2) ]; then
     killall python3
     python3 main.py
 fi
