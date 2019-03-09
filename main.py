ea0001b22359649b7f3c0e4c16b4377e39c84a04
Alatec <johnsonkids.andrew@gmail.com>
2019-03-08 22:52:49 -0700

    added circle to center

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
     cv2.circle(image, (int(center[0]), int(center[1])), 15, (255, 50, 200), thickness=cv2.FILLED)
 
-    if center[1] < 270 and center[i] > 10:
+    if center[1] < 270 and center[1] > 10:
         bobo.setTarget(TURN, 5000)
     elif center[1] > 370:
         bobo.setTarget(TURN, 7000)
