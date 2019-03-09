4d14b0796bae74da01e22e48b876b321860f52fd
Alatec <johnsonkids.andrew@gmail.com>
2019-03-08 20:06:16 -0700

    enabled auto update

--- autoUpdater.sh
+++ autoUpdater.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 cd /home/pi/Bobo/ComputerVision442
-
+sleep 30
 ssh -i "/home/pi/.ssh/id_rsa" -R 6969:localhost:22 pi@laftr.ddns.net
-#while true;
-#do
-#if [[ $(git pull | tail -n +2) ]]; then
-#    killall python3
-#    python3 main.py
-#fi
-#sleep 1
-#done
+while true;
+do
+if [[ $(git pull | tail -n +2) ]]; then
+    killall python3
+    python3 main.py
+fi
+sleep 1
+done
