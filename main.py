987a0720f66369a6c2220ca25a24c996408ffa92
cnnyra <collinsaan@gmail.com>
2019-04-03 21:45:05 -0600

    Flipped camera view

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
     image = frame.array
-    image = cv2.flip(image, 1)
+
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     print(faces)
@@ -64,6 +64,8 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         cv2.rectangle(image, (face[0], face[1]), (face[0] + face[2], face[1] + face[3]), (255, 0, 0), 2)
         print("y = " + str(face[1]) + "   x = " + str(face[0]))
         bf.findFace(face[1]+face[3]/2, face[0]+face[2]/2)
+
+    image = cv2.flip(image, 1)
     cv2.imshow("Frame", image)
 
     key = cv2.waitKey(1) & 0xFF
